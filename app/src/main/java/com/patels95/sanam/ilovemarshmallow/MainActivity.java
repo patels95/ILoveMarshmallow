75740ec83a1d0e56c223a52fc01e304a777b8131
patels95 <patels326@gmail.com>
2015-09-19 17:06:56 -0400

    added search results request

--- app/src/main/java/com/patels95/sanam/ilovemarshmallow/MainActivity.java
+++ app/src/main/java/com/patels95/sanam/ilovemarshmallow/MainActivity.java
@@ -1,14 +1,29 @@
 package com.patels95.sanam.ilovemarshmallow;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
 public class MainActivity extends Activity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     private String mUserQuery;
 
@@ -23,28 +38,76 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
         searchView.setQueryHint(getString(R.string.search_query_hint));
         // set listener to get user's search term
-        searchView.setOnQueryTextListener(searchQueryListener);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.setIconified(true);
+                searchView.clearFocus();
+                mUserQuery = query;
+                Toast.makeText(MainActivity.this, mUserQuery, Toast.LENGTH_LONG).show();
+                getSearchResults(query);
+                searchItem.collapseActionView();
+                return true;
+            }
 
-        return true;
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
-    SearchView.OnQueryTextListener searchQueryListener = new SearchView.OnQueryTextListener() {
-        @Override
-        public boolean onQueryTextSubmit(String query) {
-            mUserQuery = query;
-            Toast.makeText(MainActivity.this, mUserQuery, Toast.LENGTH_LONG).show();
-            return false;
-        }
 
-        @Override
-        public boolean onQueryTextChange(String newText) {
-            return false;
+
+    private void getSearchResults(String query) {
+        String url = "https://zappos.amazon.com/mobileapi/v1/search?term=" + query;
+
+        if (isNetworkAvailable()){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    Log.e(TAG, "search request error");
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+//                            mProducts = parseProductInfo(jsonData);
+                        }
+                        else {
+                            Log.e(TAG, "search request error");
+                        }
+                    }
+                    // add JSONException below
+                    catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
         }
-    };
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
