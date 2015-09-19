51720151fdcf35eb923bdcf927df59628c5861b7
patels95 <patels326@gmail.com>
2015-09-18 21:57:37 -0400

    added searchview

--- app/src/main/java/com/patels95/sanam/ilovemarshmallow/MainActivity.java
+++ app/src/main/java/com/patels95/sanam/ilovemarshmallow/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private String mUserQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,29 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint(getString(R.string.search_query_hint));
+        // set listener to get user's search term
+        searchView.setOnQueryTextListener(searchQueryListener);
+
         return true;
     }
+
+    SearchView.OnQueryTextListener searchQueryListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            mUserQuery = query;
+            Toast.makeText(MainActivity.this, mUserQuery, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return false;
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
